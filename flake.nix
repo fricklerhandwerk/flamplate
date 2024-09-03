ee6b6379f0fb8e9148acae847e2c91d8503d7436
Valentin Gagarin <valentin@gagarin.work>
2024-09-03 22:04:23 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = { };
+  outputs = { nixpkgs, ... }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      packages.x86_64-linux.default = pkgs.writeShellApplication {
+        name = "template";
+        text = ''
+          cp ${./example.txt} .
+        '';
+      };
+    };
+}
